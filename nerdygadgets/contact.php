c5580104159eb5c38570e5d6c73e3c9321b8eacb
Mathijs2002 <s1154985@student.windesheim.nl>
2020-11-26 15:28:36 +0100

    Contact pagina opgeschoond

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -1,91 +1,38 @@
 <?php
 include __DIR__ . "/header.php";
+
+$host = "localhost";
+$databasename = "NerdyGadgets";
+$user = "root";
+$pass = "";
+$port = 3306;
+$connection = mysqli_connect($host, $user, $pass, $databasename, $port) or die("badconnect" . mysqli_connect_error());
+
+
+
+
+$sql = "SELECT * FROM contact";
+$result = mysqli_query($connection, $sql);
+
+$contacts = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+foreach($contacts as $contact){
+    $phone = $contact["phone"];
+    $adres = $contact["address"];
+    $email = $contact["email"];
+}
+
 ?>
 
 <div class="IndexStyle">
     <div class="col-11">
-        <div class="TextMain">
+        <div class="Textcontact">
             <div class="TextPrice">
-            telefoonnummer:
-            <?php
-
-            $host = "localhost";
-            $databasename = "NerdyGadgets";
-            $user = "root";
-            $pass = "";
-            $port = 3306;
-            $connection = mysqli_connect($host, $user, $pass, $databasename, $port) or die("badconnect" . mysqli_connect_error());
-
-
-
-
-            $sql = "SELECT * FROM contact";
-            $result = mysqli_query($connection, $sql);
-
-            $contacts = mysqli_fetch_all($result,MYSQLI_ASSOC);
-
-            foreach($contacts as $contact){
-                print($contact["phone"] );
-            }
-            mysqli_close($connection);
-
-
-            ?>
+            telefoonnummer: <?php print $phone;?>
             <br>
-            email:
-            <?php
-
-            $host = "localhost";
-            $databasename = "NerdyGadgets";
-            $user = "root";
-            $pass = "";
-            $port = 3306;
-            $connection = mysqli_connect($host, $user, $pass, $databasename, $port) or die("badconnect" . mysqli_connect_error());
-
-
-
-
-            $sql = "SELECT * FROM contact";
-            $result = mysqli_query($connection, $sql);
-
-            $contacts = mysqli_fetch_all($result,MYSQLI_ASSOC);
-
-            foreach($contacts as $contact){
-                print($contact["email"] );
-            }
-            mysqli_close($connection);
-
-            ?>
-
-
+            email: <?php print $email;?>
             <br>
-            addres:
-
-            <?php
-
-            $host = "localhost";
-            $databasename = "NerdyGadgets";
-            $user = "root";
-            $pass = "";
-            $port = 3306;
-            $connection = mysqli_connect($host, $user, $pass, $databasename, $port) or die("badconnect" . mysqli_connect_error());
-
-
-
-
-            $sql = "SELECT * FROM contact";
-            $result = mysqli_query($connection, $sql);
-
-            $contacts = mysqli_fetch_all($result,MYSQLI_ASSOC);
-
-            foreach($contacts as $contact){
-                print($contact["address"] );
-            }
-            mysqli_close($connection);
-
-            ?>
-
-
+            addres: <?php print $adres;?>
             <br>
             </div>
         </div>
@@ -95,6 +42,7 @@ include __DIR__ . "/header.php";
 
 
 <?php
+mysqli_close($connection);
 include __DIR__ . "/footer.php";
 ?>
 
