b619555a360c6be4f3ad3915f9d2a09f9e4c4fd0
Redmer <spam@redmerschoenmaker.com>
2020-11-26 17:09:43 +0100

    added link in header

---
+++ nerdygadgets/profile.php
@@ -0,0 +1,25 @@
+<?php require __DIR__ . "/header.php"; ?>
+
+<?php
+
+$user_id = $_SESSION[]
+
+$query =
+
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Profiel</title>
+    <meta charset="UTF-8">
+    <style>
+
+    </style>
+</head>
+<body>
+
+<div> </div>
+
+</body>
+</html>
