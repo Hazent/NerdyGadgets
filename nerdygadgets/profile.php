19fc36284ed20eb72d532f3717f665b5c7acf3fe
Redmer <spam@redmerschoenmaker.com>
2020-12-03 15:02:22 +0100

    profile query

--- nerdygadgets/profile.php
+++ nerdygadgets/profile.php
@@ -1,12 +1,18 @@
-<?php require __DIR__ . "/header.php"; ?>
-
+<?php require __DIR__ . "/header.php";
+require __DIR__ . "/connect.php";?>
 <?php
 
-$user_id = $_SESSION[]
+$user_id = $_SESSION["userId"];
 
-$query =
+$query = "SELECT * FROM people WHERE PersonId = ?";
 
+$Statement = mysqli_prepare($Connection, $query);
+mysqli_stmt_bind_param($Statement, "i", $user_id);
+mysqli_stmt_execute($Statement);
+$Result = mysqli_stmt_get_result($Statement);
+$Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
 
+$firstName = $Result["firstname"];
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -18,7 +24,7 @@ $query =
     </style>
 </head>
 <body>
-
+<h1>Profiel</h1>
 <div> </div>
 
 </body>
